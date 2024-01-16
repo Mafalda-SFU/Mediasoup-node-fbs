830b7bbcacd69d1b688b2e0910ff9943ac6aefcb
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2024-01-16 01:48:51 +0100

    Extracted and adapted code from `@mafalda-sfu/mediasoup-node-tests`

--- README.md
+++ README.md
@@ -1,34 +1,23 @@
-# Mediasoup-node-tests
+# Mediasoup node fbs
 
-[![main](https://github.com/Mafalda-SFU/Mediasoup-node-tests/actions/workflows/main.yml/badge.svg)](https://github.com/Mafalda-SFU/Mediasoup-node-tests/actions/workflows/main.yml)
-[![npm](https://img.shields.io/npm/v/@mafalda-sfu/mediasoup-node-tests.svg)](https://www.npmjs.com/package/@mafalda-sfu/mediasoup-node-tests)
+[![main](https://github.com/Mafalda-SFU/Mediasoup-node-fbs/actions/workflows/main.yml/badge.svg)](https://github.com/Mafalda-SFU/Mediasoup-node-fbs/actions/workflows/main.yml)
+[![npm](https://img.shields.io/npm/v/@mafalda-sfu/mediasoup-node-fbs.svg)](https://www.npmjs.com/package/@mafalda-sfu/mediasoup-node-fbs)
 
-Node.js tests extracted from [Mediasoup](https://mediasoup.org/)
+Node.js flatbuffers extracted from [Mediasoup](https://mediasoup.org/)
 
-This project host some scripts to extract, update and release the tests from
-`Mediasoup` as in independent package. This is intended to validate that other
-projects implementing the `Mediasoup` API like [Mafalda SFU](https://mafalda.io)
-are compatible with the original.
+This project host some scripts to extract, update and release the flatfuffers
+from `Mediasoup` as in independent package. This is intended to ensure
+compatibility in other projects implementing the `Mediasoup` API like
+[Mafalda SFU](https://mafalda.io) are compatible with the original.
 
-The tests are automatically extracted from the
+Flatbuffers are automatically extracted from the
 [Mediasoup repository](https://github.com/versatica/mediasoup) and updated for
 each new release.
 
 ## Installation
 
 ```sh
-npm install --save-dev jest @mafalda-sfu/mediasoup-node-tests
-```
-
-## Usage
-
-```js
-import mediasoupNodeTests from '@mafalda-sfu/mediasoup-node-tests'
-
-import mediasoup from '...'
-
-
-mediasoupNodeTests(mediasoup)
+npm install --save-dev jest @mafalda-sfu/mediasoup-node-fbs
 ```
 
 ## License
--- scripts/update.js
+++ scripts/update.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const {mkdir, rm, writeFile} = require('node:fs/promises');
-const {basename, dirname, sep} = require('node:path');
+const {sep} = require('node:path');
 const {Readable} = require('node:stream');
 const {text} = require('node:stream/consumers');
 const {createGunzip} = require('node:zlib');
@@ -15,65 +15,7 @@ const tar = require('tar-stream');
 
 const options = {force: true, recursive: true}
 const repo = 'versatica/mediasoup'
-const TransportTs = `import * as FbsTransport from '../gen/fbs/transport';
 
-/**
- * Transport protocol.
- */
-export type TransportProtocol = 'udp' | 'tcp';
-
-export type TransportTuple =
-{
-  localIp: string;
-  localPort: number;
-  remoteIp?: string;
-  remotePort?: number;
-  protocol: TransportProtocol;
-};
-
-export function serializeProtocol(protocol: TransportProtocol): FbsTransport.Protocol
-{
-  switch (protocol)
-  {
-    case 'udp':
-    {
-      return FbsTransport.Protocol.UDP;
-    }
-
-    case 'tcp':
-    {
-      return FbsTransport.Protocol.TCP;
-    }
-  }
-}`
-const utilsTs = `/**
-* Make an object or array recursively immutable.
-* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze.
-*/
-export function deepFreeze<T>(object: T): T
-{
- // Retrieve the property names defined on object.
- const propNames = Reflect.ownKeys(object as any);
-
- // Freeze properties before freezing self.
- for (const name of propNames)
- {
-   const value = (object as any)[name];
-
-   if ((value && typeof value === 'object') || typeof value === 'function')
-   {
-     deepFreeze(value);
-   }
- }
-
- return Object.freeze(object);
-}`
-
-
-function filterOrtcUnsuportedError(line)
-{
-  return this.toString() !== 'ortc' || !line.includes('UnsupportedError')
-}
 
 function isNotRustRelease({tag_name})
 {
@@ -107,8 +49,6 @@ function isNotRustRelease({tag_name})
     fetch(tarball_url),
     rm('src', options)
     .then(mkdir.bind(null, 'src', options))
-    .then(writeFile.bind(null, 'src/Transport.ts', TransportTs, 'utf8'))
-    .then(writeFile.bind(null, 'src/utils.ts', utilsTs, 'utf8'))
   ])
 
   const extract = Readable.fromWeb(body).pipe(createGunzip()).pipe(tar.extract())
@@ -121,24 +61,12 @@ function isNotRustRelease({tag_name})
     path.shift()
     path = path.join(sep)
 
-    if(path === 'node/tsconfig.json')
-    {
-      path = path.split(sep)
-      path.shift()
-      path = path.join(sep)
-
-      const content = await text(entry);
-
-      await writeFile(path, content, 'utf8')
-      continue
-    }
-
     if(path.startsWith('worker/fbs'))
     {
       let path2 = path.split(sep)
       path2.shift()
       // Ensure path fragment is exactly `fbs/`, not something like `fbs...`
-      if(path2[0] === 'fbs') path2.unshift('src')
+      if(path2[0] === 'fbs') path2[0] = 'src'
       path2 = path2.join(sep)
 
       switch(type)
@@ -160,182 +88,6 @@ function isNotRustRelease({tag_name})
       }
 
       continue
-    }
-
-    if(path.startsWith('node/src/test'))
-    {
-      let path2 = path.split(sep)
-      path2.shift()
-      path2.shift()
-      // Ensure path fragment is exactly `test/`, not something like `test...`
-      if(path2[0] === 'test') path2[0] = 'src'
-      path2 = path2.join(sep)
-
-      switch(type)
-      {
-        case 'directory':
-          await mkdir(path2, options)
-          break
-
-        case 'file':
-          {
-            let content = await text(entry);
-
-            if(dirname(path2) === 'src')
-            {
-              const lines = content.split('\n')
-              content = []
-
-              let describeName
-              let imports = []
-              let indent = ''
-              let insideImport = false
-
-              for(let line of lines)
-              {
-                if(!line.length)
-                {
-                  content.push(line)
-                  continue
-                }
-
-                if(line.startsWith('import'))
-                  insideImport = true
-
-                if(insideImport)
-                {
-                  if(!line.includes('from'))
-                  {
-                    content.push(line)
-                    continue
-                  }
-
-                  insideImport = false
-
-                  // Ignore imports of mediasoup, since we are going to do
-                  // dependency injection.
-                  // TODO: generate type for `mediasoup`, or use
-                  //       `typeof mediasoup`. It's only needed for Typescript
-                  if(line.includes('mediasoup')) continue
-
-                  // Replace imports of mediasoup types.
-                  if(line.includes('../errors'))
-                  {
-                    imports.push(
-                      line
-                        .replace('import', 'const')
-                        .replace("from '../errors'", '= mediasoup.types')
-                    )
-                    continue
-                  }
-
-                  if(line.includes('../fbs'))
-                    line = line.replace('../fbs', '../gen/fbs')
-
-                  else if(line.includes('../ortc'))
-                    line = line.replace(
-                      '../ortc', '@mafalda-sfu/mediasoup-ortc'
-                    )
-
-                  else if(line.includes('../Transport'))
-                    line = line.replace('../Transport', './Transport')
-
-                  else if(line.includes('../utils'))
-                    line = line.replace('../utils', './utils')
-                }
-
-                else if(!indent)
-                {
-                  if(line.startsWith('//'))
-                  {
-                    content.push(line)
-                    continue
-                  }
-
-                  indent = '\t\t'
-
-                  if(!content[0]) content.shift()
-
-                  describeName = basename(path2, '.ts').slice(5)
-
-                  if(describeName === 'mediasoup')
-                  {
-                    content.push("import {sync} from 'pkg-dir'")
-
-                    content.push('')
-                  }
-
-                  else if(describeName === 'Worker')
-                  {
-                    content.push('const skipIfHasVirtualPids =')
-                    content.push('  process.env.HAS_VIRTUAL_PIDS')
-                    content.push('    ? test.skip')
-                    content.push('    : test')
-
-                    content.push('')
-                  }
-
-                  // TODO: generate type for `mediasoup`, or use
-                  //       `typeof mediasoup`. It's only needed for Typescript
-                  content.push('export default function(mediasoup): void')
-                  content.push('{')
-
-                  imports = imports.filter(
-                    filterOrtcUnsuportedError, describeName
-                  )
-
-                  if(imports.length)
-                  {
-                    for(const line of imports) content.push('\t' + line)
-
-                    content.push('')
-                  }
-
-                  content.push(`\tdescribe('${describeName}', () =>`)
-                  content.push('\t{')
-                }
-
-                else if(describeName === 'mediasoup')
-                {
-                  if(line.includes('__dirname'))
-                    line = line.replace(
-                      "__dirname, '..', '..', '..'", 'sync(__dirname)'
-                    )
-                }
-
-                else if(describeName === 'ortc')
-                {
-                  if(line.includes('UnsupportedError'))
-                    line = line.replace(
-                      'UnsupportedError', '/*Unsupported*/Error'
-                    )
-                }
-
-                else if(describeName === 'Worker')
-                {
-                  if(line.includes('Worker emits "died" if worker process died')
-                  || line.includes('worker process ignores PIPE, HUP, ALRM,'))
-                    line = line.replace('test', 'skipIfHasVirtualPids')
-                }
-
-                content.push(indent + line)
-              }
-
-              if(!content.at(-1)) content.pop()
-
-              content.push('\t});')
-              content.push('}')
-
-              content = content.join('\n')
-            }
-
-            await writeFile(path2, content, 'utf8')
-          }
-          break
-
-        default:
-          throw new Error(`Unknown entry type: ${type}`)
-      }
     }
 
     entry.resume()
